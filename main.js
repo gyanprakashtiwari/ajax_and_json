ac792cbb02d16052e227865bc41d89f1ad52d25f
Gyan Prakash Tiwari <gyanprakashtiwari@MacBookAir.lan>
2024-12-03 15:34:40 +0530

    update

--- main.js
+++ main.js
@@ -3,21 +3,50 @@ var animalContainer = document.getElementById("animal-info");
 var pageCounter = 1;
 
 btn.addEventListener("click", function () {
-  const url = "https://learnwebcode.github.io/json-example/animals-1.json";
+  const url =
+    "https://learnwebcode.github.io/json-example/animals-" +
+    pageCounter +
+    ".json";
   var ourRequest = new XMLHttpRequest();
   ourRequest.open("GET", url);
   ourRequest.onload = function () {
-    var ourData = JSON.parse(ourRequest.responseText);
-    renderHTML(ourData);
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    } else {
+      console.log("we connected to server , but it returned error");
+    }
   };
   ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me");
+  }
 });
 
 // this function's job is to add html to our page
 function renderHTML(data) {
   var htmlString = "";
   for (i = 0; i < data.length; i++) {
-    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+    htmlString +=
+      "<p>" + data[i].name + " is a " + data[i].species + "that likes to eat ";
+
+    for (j = 0; j < data[i].foods.likes.length; j++) {
+      if (j == 0) {
+        htmlString += data[i].foods.likes[j];
+      } else {
+        htmlString += " and " + data[i].foods.likes[j];
+      }
+    }
+    htmlString += " and dislikes ";
+    for (j = 0; j < data[i].foods.dislikes.length; j++) {
+      if (j == 0) {
+        htmlString += data[i].foods.dislikes[j];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[j];
+      }
+    }
+    htmlString += ".</p>";
   }
   animalContainer.insertAdjacentHTML("beforeend", htmlString);
 }
